68adc11d9dbaedb57306f5e16e20578ad2df44d8
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2023-11-24 17:53:51 +0530

    Defined initial project structure and styles.

---
+++ app/(Marketing)/layout.tsx
@@ -0,0 +1,5 @@
+function MarketingLayout({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
+
+export default MarketingLayout;
---
+++ app/(Marketing)/page.tsx
@@ -0,0 +1,9 @@
+function MarketingPage() {
+  return (
+    <div>
+      <h1 className="">Marketing Page</h1>
+    </div>
+  );
+}
+
+export default MarketingPage;
