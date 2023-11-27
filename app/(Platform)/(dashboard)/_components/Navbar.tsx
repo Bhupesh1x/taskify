72949671753d5132c0c58e9e760400585b36a12b
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2023-11-27 17:51:46 +0530

    Changes related to organization. and created navbar.

---
+++ app/(Platform)/(dashboard)/_components/Navbar.tsx
@@ -0,0 +1,58 @@
+import { Plus } from "lucide-react";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+
+import Logo from "@/components/shared/Logo";
+import { Button } from "@/components/ui/button";
+
+function Navbar() {
+  return (
+    <div className="fixed z-50 top-0 bg-white border-b shadow w-full h-14 px-4 flex items-center">
+      <Logo />
+      <Button
+        variant="primary"
+        size="sm"
+        className="px-2 py-1.5 rounded-sm hidden md:block ml-4"
+      >
+        Create
+      </Button>
+      <Button
+        variant="primary"
+        size="sm"
+        className="rounded-sm block md:hidden"
+      >
+        <Plus className="h-4 w-4" />
+      </Button>
+
+      <div className="ml-auto flex items-center gap-x-4">
+        <OrganizationSwitcher
+          hidePersonal
+          afterCreateOrganizationUrl="/organization/:id"
+          afterSelectOrganizationUrl="/organization/:id"
+          afterLeaveOrganizationUrl="/select-org"
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              },
+            },
+          }}
+        />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: {
+                height: 30,
+                width: 30,
+              },
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
