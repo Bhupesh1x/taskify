c96422fce450df8f469f5f9da74d6a358cdca289
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2023-12-05 17:15:22 +0530

    Created backend server action for Create card and integrated it with frontend.

--- app/(Platform)/(dashboard)/board/[boardId]/_components/CardForm.tsx
+++ app/(Platform)/(dashboard)/board/[boardId]/_components/CardForm.tsx
@@ -1,7 +1,13 @@
 "use client";
 
-import { forwardRef } from "react";
+import { toast } from "sonner";
 import { Plus, X } from "lucide-react";
+import { useParams } from "next/navigation";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
+import { useRef, forwardRef, ElementRef, KeyboardEventHandler } from "react";
+
+import { useAction } from "@/hooks/use-action";
+import { createCard } from "@/actions/create-card";
 
 import { Button } from "@/components/ui/button";
 import { FormSubmit } from "@/components/Form/FormSubmit";
@@ -16,14 +22,57 @@ type Props = {
 
 const CardForm = forwardRef<HTMLTextAreaElement, Props>(
   ({ listId, isEditing, enableEditing, disableEditing }, ref) => {
+    const params = useParams();
+    const formRef = useRef<ElementRef<"form">>(null);
+
+    const { execute, fieldError } = useAction(createCard, {
+      onSuccess: (data) => {
+        toast.success(`Card "${data.title}" created`);
+        formRef.current?.reset();
+      },
+      onError: (error) => {
+        toast.error(error);
+      },
+    });
+
+    const onkeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        disableEditing();
+      }
+    };
+
+    useOnClickOutside(formRef, disableEditing);
+    useEventListener("keydown", onkeyDown);
+
+    const onTextAreaKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = (
+      e
+    ) => {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        formRef.current?.requestSubmit();
+      }
+    };
+
+    const onSubmit = (formData: FormData) => {
+      const title = formData.get("title") as string;
+      const boardId = params.boardId as string;
+
+      execute({ title, listId, boardId });
+    };
+
     if (isEditing) {
       return (
-        <form className="m-1 py-0.5 px-1 space-y-4">
+        <form
+          ref={formRef}
+          action={onSubmit}
+          className="m-1 py-0.5 px-1 space-y-4"
+        >
           <FormTextArea
             id="title"
-            onKeyDown={() => {}}
+            onKeyDown={onTextAreaKeyDown}
             ref={ref}
             placeholder="Enter a title for this card..."
+            errors={fieldError}
           />
           <div className="flex items-center gap-x-2">
             <FormSubmit>Add card</FormSubmit>
