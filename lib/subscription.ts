53c5fb9fa214a54b86e8d7aeb8bd9ce5fb47632b
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2023-12-08 18:35:04 +0530

    Added stripe to the project and created instance of it and created a helper function for checking the subscription is valid or not

---
+++ lib/subscription.ts
@@ -0,0 +1,34 @@
+import { db } from "./db";
+
+import { auth } from "@clerk/nextjs";
+
+const DAY_IN_MS = 84_400_000;
+
+export const checkSubscription = async () => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return false;
+  }
+  const orgSubscription = await db.orgSubscription.findUnique({
+    where: {
+      orgId,
+    },
+    select: {
+      stripeCustomerId: true,
+      stripePriceId: true,
+      stripeSubscriptionId: true,
+      striptCurrentPeriodEnd: true,
+    },
+  });
+
+  if (!orgSubscription) {
+    return false;
+  }
+
+  const isValid =
+    orgSubscription.stripePriceId &&
+    orgSubscription.striptCurrentPeriodEnd?.getTime()! + DAY_IN_MS > Date.now();
+
+  return !!isValid;
+};
